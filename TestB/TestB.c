5b3e0f7f7146e6fd7c7a6f558f69f9c1a05d7085
Ben Byers <frecklefacebcb@gmail.com>
2015-12-29 14:17:05 -0700

    Add TestB Project

---
+++ TestB/TestB.c
@@ -0,0 +1,17 @@
+/*
+ * TestB.c
+ *
+ *  Created on: Dec 29, 2015
+ *      Author: ben
+ */
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	puts("Hello World!"); // Print message to console window
+
+	return 0;
+}
